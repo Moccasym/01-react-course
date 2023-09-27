f47b3fdb7ba7fcb067d528aa1d9ca86ba8eb35c3
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-09-27 21:04:13 +0200

    changed box size

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,27 @@
 import React from "react";
 import '../index.css';
-import componentsImage from '../assets/images/components.png';
-import stateImage from '../assets/images/state.png';
-import eventsImage from '../assets/images/events.png';
+
 
 const Card = (props) => {
     return(
     <div id='concepts2'>
         <ul id="concepts">
             <li className="concept">
-            <img src={componentsImage} alt={props.item[0].title} />
+            <img src={props.item[0].image} alt={props.item[0].title} />
             <h2>{props.item[0].title}</h2>
             <p>{props.item[0].description}</p>
             </li>
         </ul>
         <ul id="concepts">
             <li className="concept">
-            <img src={stateImage} alt={props.item[1].title} />
+            <img src={props.item[1].image} alt={props.item[1].title} />
             <h2>{props.item[1].title}</h2>
             <p>{props.item[1].description}</p>
             </li>
         </ul>
         <ul id="concepts">
             <li className="concept">
-            <img src={eventsImage} alt={props.item[2].title} />
+            <img src={props.item[2].image} alt={props.item[2].title} />
             <h2>{props.item[2].title}</h2>
             <p>{props.item[2].description}</p>
             </li>
